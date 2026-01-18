4042d447c678b85eebaf820127115d70185945d0
linus <132842867+einekuh@users.noreply.github.com>
2026-01-18 13:46:49 +0100

    Delete SchroedingerKI_Downloadmaterial/Dateien zum Download directory

--- SchroedingerKI_Downloadmaterial/Dateien zum Download/Samplecodes/02 Nachbarschaften/zip.py
+++
@@ -1,8 +0,0 @@
-list1 = [1, 2, 3]
-list2 = ['a', 'b', 'c']
-
-# Verwende zip, um die beiden Listen zu kombinieren
-zipped = zip(list1, list2)
-
-# Konvertiere den zip-Iterator in eine Liste und drucke sie aus
-print(list(zipped))
